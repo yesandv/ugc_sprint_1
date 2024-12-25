20bd08f8e9d894f001de757f87c7176b74ec04a4
yesandv <grc@protonmail.com>
2024-12-25 11:40:21 +0200

    Move files

--- ugc_service/src/api/v1/events.py
+++ ugc_service/src/api/v1/events.py
@@ -1,24 +1,12 @@
 from http import HTTPStatus
-from typing import Union
 
 from fastapi import APIRouter, Depends, HTTPException
 
-from ugc_service.src.schemas.event import (
-    ClickEvent,
-    EventType,
-    PageViewEvent,
-    VideoQualityEvent,
-    CompletionEvent,
-    SearchEvent,
-)
+from ugc_service.src.schemas.event import EventType, Event
 from ugc_service.src.services.event import EventService, get_event_service
 from ugc_service.src.utils.jwt import security_jwt
 
 router = APIRouter(prefix="/api/v2/events", tags=["EventService"])
-
-Event = Union[
-    ClickEvent, PageViewEvent, VideoQualityEvent, CompletionEvent, SearchEvent
-]
 
 
 @router.post(
@@ -26,7 +14,7 @@ Event = Union[
     status_code=HTTPStatus.CREATED,
     description="Create a record of a user's activity",
 )
-def record_event(
+async def record_event(
         event_type: EventType,
         event: Event,
         event_service: EventService = Depends(get_event_service),
@@ -34,6 +22,6 @@ def record_event(
 ):
     try:
         user_id = token.get("user_id")
-        event_service.record(user_id, event_type, event)
+        await event_service.record(user_id, event_type, event)
     except AssertionError:
         raise HTTPException(status_code=HTTPStatus.BAD_REQUEST)
